96ce7dd30be4358d7060e995e187a0121caf2511
Tyler Barron <tdawgg2@yahoo.com>
2022-01-26 17:14:26 -0700

    Initial Commit

--- camelup.py
+++ camelup.py
@@ -1,12 +1,240 @@
 import random, copy, math, uuid, hashlib
 from players import Player0, Player1, Player2
 from Sir_Humpfree_Bogart import Sir_Humpfree_Bogart
+import sys
+import os
+import random
+import pygame
+from pygame.locals import *
+from neat import neat
 
 camels = [0,1,2,3,4]
 num_camels = len(camels)
-num_players = 4
+num_players = 2
 finish_line = 16
 display_updates = False
+
+P0_Is_AI = True
+P1_Is_AI = False
+
+
+# Constants
+WIDTH, HEIGHT = 640, 480
+NETWORK_WIDTH = 480
+
+# Flags
+AI = True
+DRAW_NETWORK = True
+
+# Colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+YELLOW = (255, 255, 0)
+
+
+# ----------------- #
+def generate_net(current, nodes):
+    # Generate the neural network to be displayed
+    for i in current.get_nodes():
+        if current.is_input(i):
+            color = (0, 0, 255)
+            x = 50
+            y = 140 + i*60
+        elif current.is_output(i):
+            color = (255, 0, 0)
+            x = NETWORK_WIDTH-50
+            y = HEIGHT/2
+        else:
+            color = (0, 0, 0)
+            x = random.randint(NETWORK_WIDTH/3, int(NETWORK_WIDTH * (2.0/3)))
+            y = random.randint(20, HEIGHT-20)
+        nodes[i] = [(int(x), int(y)), color]
+
+def render_net(current, display, nodes):
+    # Render the current neural network
+    genes = current.get_edges()
+    for edge in genes:
+        if genes[edge].enabled: # Enabled or disabled edge
+            color = (0, 255, 0)
+        else:
+            color = (255, 0, 0)
+
+        pygame.draw.line(display, color, nodes[edge[0]][0], nodes[edge[1]][0], 3)
+
+    for n in nodes:
+        pygame.draw.circle(display, nodes[n][1], nodes[n][0], 7)
+
+def convertGamestateToInputs(g, playerPos):
+    '''
+        self.camel_track = [[] for i in range(29)]
+        self.trap_track = [[] for i in range(29)] #entry of the form [trap_type (-1,1), player]
+        self.player_has_placed_trap = [False,False,False,False]
+        self.round_bets = []		#of the form [camel,player]
+        self.game_winner_bets = []			#of the form [camel,player]
+        self.game_loser_bets = []			#of the form [camel,player]
+        self.player_game_bets = [[],[],[],[]]
+        self.player_money_values = [2,2,2,2]
+        self.camel_yet_to_move = [True,True,True,True,True]
+        self.active_game = True
+        self.game_winner = []
+        self.camels = camels
+
+
+        0	which player you are
+        1	Money p1
+        2	money p2
+        3	orange location
+        4	white location
+        5	blue location
+        6	yellow location
+        7	green location
+        8	p1 trap
+        9	p2 trap
+        10	orange has moved
+        11	white has moved
+        12	blue has moved
+        13	yellow has moved
+        14	green has moved
+        15	game winner/loser slot 1
+        16	game winner/loser slot 2
+        17	game winner/loser slot 3
+        18	game winner/loser slot 4
+        19	game winner/loser slot 5
+        20	game winner/loser slot 6
+        21	game winner/loser slot 7
+        22	game winner/loser slot 8
+        23	game winner/loser slot 9
+        24	game winner/loser slot 10
+    '''
+
+    inputMatrix = [0] * 25
+
+    inputMatrix[0] = playerPos
+    inputMatrix[1] = g.player_money_values[0]
+    inputMatrix[2] = g.player_money_values[1]
+
+    camelPos = parseCamelsPosition(g.camel_track)
+
+    inputMatrix[3] = camelPos[0]
+    inputMatrix[4] = camelPos[1]
+    inputMatrix[5] = camelPos[2]
+    inputMatrix[6] = camelPos[3]
+    inputMatrix[7] = camelPos[4]
+
+    trapPos = parseTrapsPosition(g.trap_track)
+
+    inputMatrix[8] = trapPos[0]
+    inputMatrix[9] = trapPos[1]
+
+    # Camels yet to move:
+
+    inputMatrix[10] = g.camel_yet_to_move[0]
+    inputMatrix[11] = g.camel_yet_to_move[1]
+    inputMatrix[12] = g.camel_yet_to_move[2]
+    inputMatrix[13] = g.camel_yet_to_move[3]
+    inputMatrix[14] = g.camel_yet_to_move[4]
+
+    # Game winner/loser bets
+
+    gameBets = parseGameWinnerLoserBets(g.game_winner_bets, g.game_loser_bets, playerPos)
+
+    inputMatrix[15] = gameBets[0]
+    inputMatrix[16] = gameBets[1]
+    inputMatrix[17] = gameBets[2]
+    inputMatrix[18] = gameBets[3]
+    inputMatrix[19] = gameBets[4]
+    inputMatrix[20] = gameBets[5]
+    inputMatrix[21] = gameBets[6]
+    inputMatrix[22] = gameBets[7]
+    inputMatrix[23] = gameBets[8]
+    inputMatrix[24] = gameBets[9]
+
+    return inputMatrix
+
+
+def convertOutputToAction(output):
+    '''
+        Takes one of the 48 outputs and turns it into the action form that the game expects
+
+        Roll the die	
+        Make a round winner bet	
+            5, 1 for each camel
+        Make a game winner / loser bet	
+            5 x 2, 1 for each camel, 1 for each option
+        Place a trap	
+            16 x 2, 1 for each space, 1 for each option
+
+        result[0] == 0: #Player wants to move camel
+        result[0] == 1: #Player wants to place trap
+            if g.player_has_placed_trap[player]: MoveTrap(g,result[1],result[2],player)
+        result[0] == 2: #Player wants to make round winner bet
+            PlaceRoundWinnerBet(g,result[1],player)
+        result[0] == 3: #Player wants to make game winner bet
+            PlaceGameWinnerBet(g,result[1],player)
+        result[0] == 4: #Player wants to make game loser bet
+            PlaceGameLoserBet(g,result[1],player)
+    '''
+
+    thresh = 0.8
+
+    # roll the die
+    if output[0] and output[0] > thresh: return [0]
+
+    # Make a round winner bet	
+    if output[1] and output[1] > thresh: return [2,0]
+    if output[2] and output[2] > thresh: return [2,1]
+    if output[3] and output[3] > thresh: return [2,2]
+    if output[4] and output[4] > thresh: return [2,3]
+    if output[5] and output[5] > thresh: return [2,4]
+
+    # Make a game winner / loser bet	
+    if output[6] and output[6] > thresh: return [3,0]
+    if output[7] and output[7] > thresh: return [3,1]
+    if output[8] and output[8] > thresh: return [3,2]
+    if output[9] and output[9] > thresh: return [3,3]
+    if output[10] and output[10] > thresh: return [3,4]
+
+    # Place a trap	
+    #   forwards
+    if output[11] and output[11] > thresh: return [1,1,1]
+    if output[12] and output[12] > thresh: return [1,1,2]
+    if output[13] and output[13] > thresh: return [1,1,3]
+    if output[14] and output[14] > thresh: return [1,1,4]
+    if output[15] and output[15] > thresh: return [1,1,5]
+    if output[16] and output[16] > thresh: return [1,1,6]
+    if output[17] and output[17] > thresh: return [1,1,7]
+    if output[18] and output[18] > thresh: return [1,1,8]
+    if output[19] and output[19] > thresh: return [1,1,9]
+    if output[20] and output[20] > thresh: return [1,1,10]
+    if output[21] and output[21] > thresh: return [1,1,11]
+    if output[22] and output[22] > thresh: return [1,1,12]
+    if output[23] and output[23] > thresh: return [1,1,13]
+    if output[24] and output[24] > thresh: return [1,1,14]
+    if output[25] and output[25] > thresh: return [1,1,15]
+
+    #   backwards
+    if output[26] and output[26] > thresh: return [1,-1,1]
+    if output[27] and output[27] > thresh: return [1,-1,2]
+    if output[28] and output[28] > thresh: return [1,-1,3]
+    if output[29] and output[29] > thresh: return [1,-1,4]
+    if output[30] and output[30] > thresh: return [1,-1,5]
+    if output[31] and output[31] > thresh: return [1,-1,6]
+    if output[32] and output[32] > thresh: return [1,-1,7]
+    if output[33] and output[33] > thresh: return [1,-1,8]
+    if output[34] and output[34] > thresh: return [1,-1,9]
+    if output[35] and output[35] > thresh: return [1,-1,10]
+    if output[36] and output[36] > thresh: return [1,-1,11]
+    if output[37] and output[37] > thresh: return [1,-1,12]
+    if output[38] and output[38] > thresh: return [1,-1,13]
+    if output[39] and output[39] > thresh: return [1,-1,14]
+    if output[40] and output[40] > thresh: return [1,-1,15]
+
+
+    # uh oh we failed to do anything, just roll
+    return [0]
+
+
+# ---------------------- #
 
 class GameState:
     def __init__(self):
@@ -37,8 +265,13 @@ class PlayerInterface():
         raise NotImplementedError( "Should have implemented this" )
 
 
-def PlayGame(player0,player1,player2,player3):
-    players = [player0.move,player1.move,player2.move,player3.move]
+def PlayGame(player0,player1):
+    if P0_Is_AI and P1_Is_AI:
+        players = [player0, player1]
+    elif P0_Is_AI and not P1_Is_AI:
+        players = [player0,player1.move]
+    else:
+        players = [player0.move,player1.move]
 
     def action(result,player):
         if display_updates: print("Player Action: " + str(result))
@@ -68,8 +301,29 @@ def PlayGame(player0,player1,player2,player3):
     g = GameState()
     g_round = 0
     while g.active_game:
-        active_player = (g_round%4)
-        action(players[active_player](active_player,copy.deepcopy(g)),active_player)
+        active_player = (g_round%2)
+        
+        if P0_Is_AI and active_player == 0:
+            # Set our inputs
+            inputs = convertGamestateToInputs(g, active_player)
+
+            # Check our outputs
+            output = player0.forward(inputs)
+
+            #call action with that output
+            actionValue = convertOutputToAction(output)
+            action(actionValue, 0)
+
+        elif P1_Is_AI and active_player == 1:
+            # Set our inputs
+            # Check our outputs
+            output = player1.forward(inputs)[0]
+
+            #call action with that output
+        
+        else:
+            action(players[active_player](active_player,copy.deepcopy(g)),active_player)
+
         g_round += 1
         if display_updates:
             DisplayGamestate(g)
@@ -222,11 +476,11 @@ def EndOfGame(g):
 
     payout_index = 0 #Settle bets on losing camel
     for i in range(0,len(g.game_loser_bets) - 1):
-        if check_bet(g.game_loser_bets[i][0],str(winning_camel)): #if you win, get prize
+        if check_bet(g.game_loser_bets[i][0],str(losing_camel)): #if you win, get prize
             payout = str(payout_struct[payout_index] if payout_index < len(payout_struct) else 1)
             g.player_money_values[g.game_loser_bets[i][1]] += payout_struct[payout_index]
             if display_updates : print("Paid Player " + str(g.game_loser_bets[i][1]) + " "  + " coins for selecting the game loser")
-            payout_index += 1 #decrease value of guessing winning camel
+            payout_index += 1 #decrease value of guessing losing camel
         else:
             g.player_money_values[g.game_loser_bets[i][1]] -= 1
             if display_updates : print("Paid Player " + str(g.game_loser_bets[i][1]) + " -1 coins for incorrectly selecting the game loser")
@@ -248,6 +502,81 @@ def FindCamelInNthPlace(track,n):
             camels_counted += camels_in_stack
             i += 1
     return False
+
+def parseCamelsPosition(track):
+    '''
+    Take in all the whole track and return in a matrix the parsed positions of each camel
+    "050" = on square 5 in the zero position
+    "051" = on top of 50
+    142 is another example
+    '''
+
+    camelPos = [0] * num_camels
+
+    trackPos = 0
+    for place in track:
+        stackPos = 0
+        for camel in place:
+            camelPos[camel] = trackPos * 10 + stackPos
+            stackPos += 1
+        trackPos += 1
+    return camelPos
+
+def parseTrapsPosition(track):
+    '''
+    Take in all the whole trap track and return in a matrix the parsed positions of each trap
+        if its a speed up or slow down		1 = speed up	2 = slow down
+        Where the traps are		2 dig number with it's location	
+
+        #entry of the form [trap_type (-1,1), player]
+    '''
+
+    trapPos = [0] * num_players
+
+    trackPos = 0
+    for place in track:
+        if place:
+            trapPos[place[1]] = trackPos
+            if place[0] == -1:
+                trapPos[place[1]] += 200
+            else:
+                trapPos[place[1]] += 100
+        trackPos += 1
+    return trapPos
+
+def parseGameWinnerLoserBets(winnerBets, loserBets, playerNum):
+    '''
+    Take in winner and loser bets and parses it into a form that the input matrix will like
+        5x for each player			
+        "each slot says if they voted on a winner/loser
+        and who it was OR the opponent voted on a winner/loser"			
+        1 = it was you, 2 it wasn't you	1 = winner, 2 = loser	1= orange, 2 = white, 3 = blue, etc, 9 = idk
+
+        self.game_winner_bets = []			#of the form [camel,player]
+        self.game_loser_bets = []			#of the form [camel,player]
+    '''
+
+    bets = [0] * 10
+    
+    betPointer = 0
+    for gameWinBet in winnerBets:
+        if gameWinBet[1] == playerNum:
+            bets[betPointer] = 110 + int(gameWinBet[0])
+        else:
+            bets[betPointer] = 200 + 10 + 9
+        
+        betPointer += 1
+    
+    betPointer = 9
+    for gameLosebet in loserBets:
+        if gameLosebet[1] == playerNum:
+            bets[betPointer] = 120 + int(gameLosebet[0])
+        else:
+            bets[betPointer] = 200 + 20 + 9
+        
+        betPointer -= 1
+
+    return bets
 
 def DisplayGamestate(g):
     print("Track:")
@@ -300,24 +629,138 @@ def DisplayTrackState(track):
     print(track_label_string)
 
 def hash_bet(bet):
+    return bet # TODO: make this better or something? I'm subverting the hashing
     # uuid is used to generate a random number
     salt = uuid.uuid4().hex
     return hashlib.sha256(salt.encode() + bet.encode()).hexdigest() + ':' + salt
     
 def check_bet(hashed_bet, user_bet):
+    return hashed_bet == user_bet # TODO: make this better or something? I'm subverting the hashing
     bet, salt = hashed_bet.split(':')
     return bet == hashlib.sha256(salt.encode() + user_bet.encode()).hexdigest()
 
 
-player_pool = [Player0, Player1, Player2, Sir_Humpfree_Bogart]
-player_points = [0 for i in range(len(player_pool))]
+# games_to_play = 20
+# player_pool = [Player0, Player1]
+# player_points = [0 for i in range(len(player_pool))]
 
-for game in range(math.ceil(len(player_pool)/10)*100):
-    order = [i for i in range(len(player_pool))]
-    random.shuffle(order)
-    winner = PlayGame(player_pool[order[0]],player_pool[order[1]],player_pool[order[2]],player_pool[order[3]])
-    for num_winners in winner: player_points[order[num_winners]] += 1
-    if sum(player_points)%10 == 0:
-        print("---")
-        for i in range(len(player_pool)):
-            print("Player " + str(player_pool[i]) + " has " + str(player_points[i]) + " points.")
+# for game in range(math.ceil(len(player_pool)/10)*games_to_play):
+#     order = [i for i in range(len(player_pool))]
+#     random.shuffle(order)
+#     winner = PlayGame(player_pool[order[0]],player_pool[order[1]])
+#     for num_winners in winner: player_points[order[num_winners]] += 1
+#     if sum(player_points)%10 == 0:
+#         print("---")
+#         for i in range(len(player_pool)):
+#             print("Player " + str(player_pool[i]) + " has " + str(player_points[i]) + " points.")
+
+
+
+def main():
+    # Main game function
+    if DRAW_NETWORK:
+        pygame.init()
+        display = pygame.display.set_mode((NETWORK_WIDTH, HEIGHT), 0, 32)
+        network_display = pygame.Surface((NETWORK_WIDTH, HEIGHT))
+        clock = pygame.time.Clock()
+
+
+    # Load the camel's brain
+    if os.path.isfile('camelupbrain_0.neat'):
+        P0Brain = neat.Brain.load('camelupbrain_0')
+    else:
+        hyperparams = neat.Hyperparameters()
+        hyperparams.delta_threshold = 0.75
+
+        hyperparams.mutation_probabilities['node'] = 0.05
+        hyperparams.mutation_probabilities['edge'] = 0.05
+
+        P0Brain = neat.Brain(25, 48, 100, hyperparams)
+        P0Brain.generate()
+    
+    # Load the old camel's brain
+    if os.path.isfile('camelupbrain_1.neat'):
+        P1Brain = neat.Brain.load('camelupbrain_1')
+        P1_Is_AI = True
+    else:
+        P1_Is_AI = False
+        P1Current = Player0
+
+    P0Current = P0Brain.get_current()
+    inputs = [0] * 25
+    output = [0] * 48
+    nodes = {}
+
+    generate_net(P0Current, nodes)
+
+    if P1_Is_AI:
+        P1Current = P1Brain.get_current()
+        generate_net(P1Current, nodes)
+
+
+    print("Loaded... starting")
+
+    gamesPlayed = 0
+    while True:
+        # Main loop
+        if DRAW_NETWORK: 
+            display.fill(BLACK)
+            network_display.fill(WHITE)
+
+        games_to_play = 20
+        player_pool = [P0Current, P1Current]
+        player_points = [0 for i in range(len(player_pool))]
+
+        for game in range(math.ceil(len(player_pool)/10)*games_to_play):
+            order = [i for i in range(len(player_pool))]
+            # random.shuffle(order) #TODO: Right now we aren't shuffling the order for my SANITY. We'll change that later
+            winner = PlayGame(player_pool[order[0]],player_pool[order[1]])
+            for num_winners in winner: player_points[order[num_winners]] += 1
+            
+        # for i in range(len(player_pool)):
+        #     print("Player " + str(player_pool[i]) + " has " + str(player_points[i]) + " points.")
+
+        P0Current.set_fitness(float(player_points[0] / games_to_play))
+
+        if DRAW_NETWORK:
+            render_net(P0Current, network_display, nodes)
+        if gamesPlayed % 10 == 0:
+            print("Fitness: ", float(player_points[0] / games_to_play))
+            print("Generation: ", P0Brain.get_generation()+1)
+            
+            if P1_Is_AI:
+                P0Brain.save('camelupbrain_1')
+                P0Current.set_fitness(0)
+                P1Brain = neat.Brain.load('camelupbrain_1')
+                P1Current = P1Brain.get_current()
+                generate_net(P1Current, nodes)
+
+        # These happen on a game end
+
+        # Save the bird's brain
+        P0Brain.save('camelupbrain_0')
+        P0Brain.next_iteration()
+        P0Current = P0Brain.get_current()
+
+        # Restart the game
+        generate_net(P0Current, nodes)
+
+        # Update display and its caption
+        if DRAW_NETWORK:
+            display.blit(network_display, (WIDTH, 0))
+            pygame.display.set_caption("GNRTN : {0}; SPC : {1}; CRRNT : {2}; FIT : {3}".format(
+                                                P0Brain.get_generation()+1, 
+                                                P0Brain.get_current_species()+1, 
+                                                P0Brain.get_current_genome()+1,
+                                                P0Brain.get_fittest().get_fitness(),
+                                                # round(output, 48),
+                                                # [round(i, 48) for i in inputs]
+            ))
+            pygame.display.update()
+            clock.tick(1)
+
+        gamesPlayed += 1
+
+
+if __name__ == "__main__":
+    main()
